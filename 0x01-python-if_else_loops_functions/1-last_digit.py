1d8e95f8c2fd085747784881f4663a1b17690e90
der-bew <derbew12@gmail.com>
2021-11-16 18:29:06 +0300

    Fixed: Negative num output error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 ld = number % 10 if number >= 0 else (number * -1) % 10
 if ld > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5"
-          .format(number, ld))
-elif ld is 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number, ld))
+    print("Last digit of {:d} is {:d} and is greater than {:d}"
+          .format(number, ld, 5))
+elif ld == 0:
+    print("Last digit of {:d} is {:d} and is {:d}".format(number, ld, 0))
 elif ld < 6 and ld != 0:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
-          .format(number, ld))
+    print("Last digit of {:d} is {:d} and is less than {:d} and not {:d}"
+          .format(number, ld, 6, 0))
