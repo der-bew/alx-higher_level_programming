758ca2b60a01d1dc9ca4926f905b82890150704c
der-bew <derbew12@gmail.com>
2021-11-22 16:36:13 +0300

    Add: 8-multiple_returns.py file

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,13 @@
+def multiple_returns(sentence):
+    '''
+    INPUT:
+      sentence: input sentence
+
+    Function that returns a tuple with the length of a string and its
+    first character
+    '''
+    my_tuple = tuple(sentence)
+    if len(my_tuple) == 0:
+        return len(my_tuple), None
+    else:
+        return len(my_tuple), my_tuple[0]
