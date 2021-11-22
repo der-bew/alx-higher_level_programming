7967f041b66e9172d88edd32a436f0485c12a2ba
der-bew <derbew12@gmail.com>
2021-11-22 16:41:07 +0300

    Add: 8-multiple_returns.py file

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -8,6 +8,6 @@ def multiple_returns(sentence):
     '''
     my_tuple = tuple(sentence)
     if len(my_tuple) == 0:
-        return len(my_tuple), None
+        return (len(my_tuple), None)
     else:
-        return len(my_tuple), my_tuple[0]
+        return (len(my_tuple), my_tuple[0])
