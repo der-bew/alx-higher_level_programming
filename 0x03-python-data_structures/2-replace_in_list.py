931bf2189eea2571783fea38bb9d4cb7dd05375a
der-bew <derbew12@gmail.com>
2021-11-22 14:39:01 +0300

    Fixed: output error

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    """Replaces an element of a list at a specific position
-    If idx is negative, the function should not modify anything,
-    and returns the original list
-    If idx is out of range (> of number of element in my_list), the 
-    function should not modify anything, and returns the original list
-    You are not allowed to import any module
-    You are not allowed to use try/except
-    """
+    '''
+    INPUT:
+       my_list: input list
+       idx: the index number
+       element: the element to be inserted at the given idx
 
+    Replaces an element of a list at a specific position
+    '''
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
         my_list[idx] = element
         return my_list
-
-    
