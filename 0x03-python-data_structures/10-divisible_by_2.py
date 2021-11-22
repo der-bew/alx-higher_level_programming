332d5fedde235b46b65ba1c99ca0f7f8d787e475
der-bew <derbew12@gmail.com>
2021-11-22 17:27:02 +0300

    Add: 10-divisible_by_2.py file

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -8,6 +8,5 @@ def divisible_by_2(my_list=[]):
 
     Function that finds all multiples of 2 in a list
     '''
-    if my_list:
-        new_list = [False if x % 2 else True for x in my_list]
+    new_list = [False if x % 2 else True for x in my_list]
     return new_list
