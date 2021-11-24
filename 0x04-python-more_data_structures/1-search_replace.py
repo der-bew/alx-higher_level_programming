e58c155897114de5b441a8bdbbf5f47b9d7c33ae
der-bew <derbew12@gmail.com>
2021-11-24 19:19:26 +0300

    Fixed: pycodestyle error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -7,7 +7,7 @@ def search_replace(my_list, search, replace):
       replace: is the new element
     RETURN: new list
 
-    A function that replaces all occurrences of an element by another 
+    A function that replaces all occurrences of an element by another
     in a new list
     '''
     return list(map(lambda x: x if x != search else replace, my_list))
