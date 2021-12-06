4598fbd9a8129f61fe9695595b801114e70af7d8
der-bew <derbew12@gmail.com>
2021-12-06 18:14:52 +0300

    Fixed: Output error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-
-from sys import stderr
 def safe_print_integer_err(value):
-    ''' prints an Integer '''
+    '''prints an Integer'''
+    from sys import stderr
     try:
         print("{:d}".format(value))
         return True
     except (TypeError, ValueError) as e:
-        print("Exception: {}".format(e), file = stderr)
+        print("Exception: {}".format(e), file=stderr)
         return False
