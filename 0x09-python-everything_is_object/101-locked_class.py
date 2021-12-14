d11dac82449dfacf9f0e4496e4e580fb0aa4365e
der-bew <derbew12@gmail.com>
2021-12-14 23:08:38 +0300

    Fixed some pycodestyle issue

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+""" Low memory cost """
+
+
 class LockedClass:
-    """ locked class """
+    """ locked class created with lock atrr """
+
     __slots__ = ['first_name']
+
+    def __init__(self, first_name=''):
+        """ method with a first_name atrr """
+        self.first_name = first_name
