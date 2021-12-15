62c385529a65850b507080613c13d54aef20deb7
der-bew <derbew12@gmail.com>
2021-12-15 11:16:14 +0300

    Fixed: Shebang error

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,4 +1,4 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 """
 This is the "3-say_my-name" module.
 The 3-say_my_name  module supplies one function, say_my_name.
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,4 +1,4 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 """
 This is the "5-test_indentation" module.
 The 5-text_indentation module supplies one function, text_indentation(text).
