f060c845bf4eb58330f48ddfb2c66dcf7165b671
der-bew <derbew12@gmail.com>
2021-12-15 11:29:03 +0300

    Fixed some output error

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -20,7 +20,7 @@ def matrix_mul(m_a, m_b):
             if l2 == 0:
                 raise ValueError("m_a can't be empty")
         if l2 != len(i):
-            raise TypeError("each row of m_a must be of the same size")
+            raise TypeError("each row of m_a must should be of the same size")
         for j in i:
             if type(j) is not int and type(j) is not float:
                 raise TypeError("m_a should contain only integers or floats")
@@ -37,7 +37,7 @@ def matrix_mul(m_a, m_b):
             if l3 == 0:
                 raise ValueError("m_b can't be empty")
         if l3 != len(i):
-            raise TypeError("each row of m_b must be of the same size")
+            raise TypeError("each row of m_b must should be of the same size")
         for j in i:
             if type(j) is not int and type(j) is not float:
                 raise TypeError("m_b should contain only integers or floats")
