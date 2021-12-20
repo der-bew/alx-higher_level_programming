f17e3b385fc88c18c74a639fd47679701ccc9536
der-bew <derbew12@gmail.com>
2021-12-20 17:00:55 +0300

    Update: 9-rectangle.py file

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,41 +1,36 @@
 #!/usr/bin/python3
 """
-Creates a Rectangle class.
+Contains the class BaseGeometry and subclass Rectangle
 """
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class BaseGeometry:
+    """A class with public instance methods area and integer_validator"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates that value is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
-    """Represents a rectangle.
-    Private instance attributes:
-        - width
-        - height
-    Public method area().
-    Inherits from BaseGeometry.
-    """
-
+    """A representation of a rectangle"""
     def __init__(self, width, height):
-        """Initializes an instance.
-        Args:
-            - width: width of the rectangle
-            - heigth: height of the rectangle
-        """
-
+        """instantiation of the rectangle"""
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
 
-    def __str__(self):
-        """Returns a formatted string."""
-
-        return str("[Rectangle] {}/{}".format(self.__width, self.__height))
-
     def area(self):
-        """Computes the area of the Rectangle instance.
-        Overwrites the area() method from BaseGeometry.
-        """
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
 
-        return self.__width * self.
+    def __str__(self):
+        """informal string representation of the rectangle"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
