20dc279d6ebedbe903c88e851cc9fe5eeea1b26b
der-bew <derbew12@gmail.com>
2021-12-21 22:44:14 +0300

    Add: 9-student.py file

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -5,25 +5,19 @@ Creates a Student class.
 
 
 class Student:
-    """Class that defines a student.
-    Public attributes:
-        - first_name
-        - last_name
-        - age
-    Public method to_json().
-    """
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes the Student instance."""
-
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Retrieves a dictionary representation
-        of a Student instance.
-        Returns: the dict representation of the instance.
-        """
-
-        return self.
+        """Get a dictionary representation of the Student."""
+        return self.__dict__
