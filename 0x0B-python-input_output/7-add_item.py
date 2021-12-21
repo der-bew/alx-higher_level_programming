ab5cb8cd5a2f8bcbb036b37458ef47d13c58bcd9
der-bew <derbew12@gmail.com>
2021-12-21 15:58:05 +0300

    Add: 7-add_item.py file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
 """
-Adds all arguments to a Python list,
-and then save them to a file.
+Script that adds all arguments to a Python list, and then saves them to a file
 """
 
-import sys
-import json
-import os.path
+from sys import argv
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+filename = "add_item.json"
 
-my_file = 'add_item.json'
+try:
+    json_list = load_from_json_file(filename)
+except OSError:
+    json_list = []
 
-my_list = []
+joson_list = [arg for arg in argv[1:]]
 
-if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
-    my_list = load_from_json_file(my_file)
-
-if len(sys.argv) > 1:
-    my_list = [elem for elem in sys.argv[1:]]
-
-save_to_json_file(my_list, my_file)
+save_to_json_file(json_list, filename)
