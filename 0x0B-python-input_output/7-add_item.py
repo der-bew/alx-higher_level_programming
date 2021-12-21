675ca10b001eacaed902acd5e6275140d14526d3
der-bew <derbew12@gmail.com>
2021-12-21 22:30:38 +0300

    Fixed: exception error

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
 """
-Adds all arguments to a Python list,
-and then save them to a file.
+Script that adds all arguments to a Python list, and then saves them to a file
 """
 
-import sys
-import json
-import os.path
-
+from sys import argv
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-my_file = 'add_item.json'
+filename = "add_item.json"
 
-my_list = []
+try:
+    json_list = load_from_json_file(filename)
+except OSError:
+    json_list = []
 
-if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
-    my_list = load_from_json_file(my_file)
+json_list = [elem for elem in argv[1:]]
 
-if len(sys.argv) > 1:
-    my_list = [elem for elem in sys.argv[1:]]
-
-save_to_json_file(my_list, my_file)
+save_to_json_file(json_list, filename)
