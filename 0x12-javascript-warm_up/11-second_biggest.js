ae5612d91c7ddb6c902a6fc8cf9657bc7bd2466d
der-bew <derbew12@gmail.com>
2022-02-28 23:27:08 +0300

    Fix Checker 5 error . 11-second_biggest.js file

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-const arr = process.argv.slice(2);
-if (arr.length > 1) {
-  arr.sort();
-  const n = arr[arr.length - 2];
-  console.log(n);
+if (process.argv.length <= 3) {
+  console.log('0');
 } else {
-  console.log(0);
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
