b03bdf6db68022e4632a2fe8cd0297653653e4e8
der-bew <derbew12@gmail.com>
2022-02-28 22:57:52 +0300

    Add: 11-second_biggest.js file

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-
-let n = 0;
+'use strict';
+let nextMax = 0;
 const args = process.argv.slice(2);
 if (args.length > 1) {
   args.sort();
-  n = args[args.length - 2];
-} else {
-  console.log(n);
+  nextMax = args[args.length - 2];
 }
+console.log(nextMax);
