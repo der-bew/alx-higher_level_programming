39188b3a410e0bb17f633d175abe1044589f9d2d
der-bew <derbew12@gmail.com>
2022-03-06 11:10:14 +0300

    Add: 100-move_to_utf8.sql file

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,4 +6,3 @@
 
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8_unicode_ci;
-ALTER TABLE first_table CHANGE name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8_unicode_ci;
