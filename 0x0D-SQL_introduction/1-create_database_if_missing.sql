8d2af6fd7f87a7b1be4c3996e6dcac52ff16e58c
der-bew <derbew12@gmail.com>
2022-03-06 08:51:34 +0300

    Add: 1-create_database_if_missing.sql file

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -2,4 +2,4 @@
 -- If the db hbtn_0c_0 already exists, ur script should not fail
 -- you are not allowed to use the SELECT or SHOW statements
 
-CREATE DATABASE hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
