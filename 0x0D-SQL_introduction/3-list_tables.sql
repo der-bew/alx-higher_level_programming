72c53d614c864e508bf03eca4280d590a249b276
der-bew <derbew12@gmail.com>
2022-03-06 09:05:16 +0300

    Add: 3-list_tables.sql file

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,5 @@
+-- lists all the tables of a database in your MySQL server.
+-- The database name will be passed as argument of mysql command
+-- (in the following example: mysql is the name of the database)
+
+SHOW TABLES;
