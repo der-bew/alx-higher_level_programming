041bc53a126c78f763b1765d346026b0e487f145
der-bew <derbew12@gmail.com>
2022-03-06 11:38:06 +0300

    Add: 101-avg_temperatures.sql file

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Displays the average temperature by city ordered by temperature (desc)
-SELECT city, ROUND(AVG(value) AS avg_temp, 4)
+SELECT city, AVG(value) AS avg_temp
    FROM temperatures
    GROUP BY city
    ORDER BY avg_temp;
