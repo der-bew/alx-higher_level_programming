bb341aacf462372ba7e934adec54407380efa729
der-bew <derbew12@gmail.com>
2022-03-06 09:33:57 +0300

    Add: 7-insert_value.sql file

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,7 @@
+-- inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+    -- New row:
+       -- id = 89
+       -- name = Best School
+   -- The database name will be passed as an argument of the mysql command
+
+INSERT INTO first_table values(89, 'Best School');
