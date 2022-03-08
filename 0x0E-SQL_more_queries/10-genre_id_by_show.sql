90d9062c59cec264a4de81520150c47ca32f3f0e
der-bew <derbew12@gmail.com>
2022-03-08 12:47:25 +0300

    Add: 10-genre_id_by_show.sql file

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -6,5 +6,5 @@
 
 SELECT ts.title, tsg.genre_id
    FROM tv_shows AS ts
-   INNER JOIN tv_show_genres AS tsg ON tsg.tv_show_genres_ibfk_1 = ts.id
+   INNER JOIN tv_show_genres AS tsg ON tsg.show_id = ts.id
    ORDER BY ts.title, tsg.genre_id;
