3c987a0a24eca2e556f0c9ab0eb1e1e6275a9b3f
der-bew <derbew12@gmail.com>
2022-03-08 12:52:41 +0300

    Add: 11-genre_id_all_shows.sql file

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,11 @@
+-- lists all shows contained in the database hbtn_0d_tvshows.
+   -- Each record should display: tv_shows.title - tv_show_genres.genre_id
+   -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+   -- If a show doesn’t have a genre, display NULL
+   -- You can use only one SELECT statement
+   -- The database name will be passed as an argument of the mysql command
+
+SELECT ts.title, tsg.genre_id
+   FROM tv_shows AS ts
+   LEFT JOIN tv_show_genres AS tsg ON tsg.show_id = ts.id
+   ORDER BY ts.title, tsg.genre_id;
