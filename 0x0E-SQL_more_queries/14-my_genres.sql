3217c8b6454ba9da0edc803acafa7b201851583e
der-bew <derbew12@gmail.com>
2022-03-08 13:21:19 +0300

    Add: 14-my_genres.sql file

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,15 @@
+-- lists all genres of the show Dexter.
+   -- The tv_shows table contains only one record where title = Dexter (but the id can be different)
+   -- Each record should display: tv_genres.name
+   -- Results must be sorted in ascending order by the genre name
+   -- You can use only one SELECT statement
+   -- The database name will be passed as an argument of the mysql command
+
+SELECT tg.name
+   FROM tv_genres AS tg
+   INNER JOIN tv_show_genres AS tsg
+   ON tsg.genre_id = tg.id
+   INNER JOIN tv_shows AS ts
+   ON tsg.show_id = ts.id
+   WHERE ts.title = "Dexter"
+   ORDER BY tg.name;
