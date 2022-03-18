3c44b4db3999d59d53a32ef61b163721046c89df
der-bew <derbew12@gmail.com>
2022-03-18 22:55:42 +0300

    update: 4-hbtn_status.py file

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
-
-
-if __name__ == "__main__":
+"""
+fetches https://intranet.hbtn.io/status
+"""
+if __name__ == '__main__':
     import requests
-
     r = requests.get('https://intranet.hbtn.io/status')
-
-    print('Body response:')
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
+    text = r.text
+    print("Body response:")
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
