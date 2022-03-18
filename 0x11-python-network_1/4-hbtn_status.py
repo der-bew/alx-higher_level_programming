c5853c46fbde8681dac9efe38cba79e3f3d11c6d
der-bew <derbew12@gmail.com>
2022-03-19 00:13:00 +0300

    Update: 4-hbtn_status.py file

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-fetches https://intranet.hbtn.io/status
+Python script that fetches an URL with requests package
 """
-if __name__ == '__main__':
-    import requests
+import requests
+
+
+if __name__ == "__main__":
     r = requests.get('https://intranet.hbtn.io/status')
-    text = r.text
-    print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
