8181a26120b2e3a0143971fec541b4db58d4bf69
der-bew <derbew12@gmail.com>
2022-03-18 22:43:01 +0300

    Update: 0-hbtn_status.py file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""
-fetches https://intranet.hbtn.io/status
-"""
+"""Fetches https://intranet.hbtn.io/status"""
+
+
 if __name__ == "__main__":
-    import urllib.request as request
-    with request.urlopen('https://intranet.hbtn.io/status') as r:
-        html = r.read()
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
         print('Body response:')
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
