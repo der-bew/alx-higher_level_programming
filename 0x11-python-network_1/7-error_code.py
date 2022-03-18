99388c028c51248d2a8399b6251f7412dbdd4133
der-bew <derbew12@gmail.com>
2022-03-18 23:18:03 +0300

    Add: 7-error_code.py file

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""
-takes in a URL and an email address, sends a POST request to the passed URL
-with the email as a parameter, and finally displays the body of the response
-"""
-if __name__ == '__main__':
-    import requests
-    from sys import argv
-    payload = {'email': argv[2]}
-    r = requests.post(argv[1], data=payload)
-    print(r.text)
+"""Takes in a URL, sends a request to the URL and
+displays the body of the response"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    res = requests.get(argv[1])
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)
