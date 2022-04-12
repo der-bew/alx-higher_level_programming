1dd6b6115cc8ca6dfe90e2805124ab93e7cfc6cd
der-bew <derbew12@gmail.com>
2022-04-12 16:12:26 +0300

    Add: 3-starwars_title.js file

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,9 +2,11 @@
 // prints the title of a Star Wars movie where the episode num matches given int
 
 const request = require('request');
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
   if (error) {
     console.error(error);
+  } else {
+    console.log(JSON.parse(body).title);
   }
-  console.log(JSON.parse(body).title);
 });
