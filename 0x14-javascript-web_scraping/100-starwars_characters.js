db6951f101671c159450aea1776b196705517565
der-bew <derbew12@gmail.com>
2022-04-13 22:27:32 +0300

    Add: 100-starwars_characters.js file

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
+request('https://swapi-api.hbtn.io/api/films' + process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
   }
